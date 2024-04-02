ffda33d7deafb2243573a4a0f1faf9bcff461edc
jkasser <jkasser@gmail.com>
2024-04-02 08:51:22 -0400

    retry if we get rate limited

--- src/components/AnalyzerForm.js
+++ src/components/AnalyzerForm.js
@@ -28,7 +28,7 @@ function PlaylistAnalyzer({ accessToken }) {
             let nextPage = `https://api.spotify.com/v1/users/${enteredUsername}/playlists`;
 
             while (nextPage) {
-                const response = await fetch(nextPage, {
+                const response = await fetchWithRetries(nextPage, {
                     method: 'GET',
                     headers: {
                         'Authorization': 'Bearer ' + accessToken
@@ -71,7 +71,7 @@ function PlaylistAnalyzer({ accessToken }) {
             let nextPage = `https://api.spotify.com/v1/playlists/${selectedPlaylist}/tracks?market=US`;
 
             while (nextPage) {
-                const response = await fetch(nextPage, {
+                const response = await fetchWithRetries(nextPage, {
                     method: 'GET',
                     headers: {
                         'Authorization': 'Bearer ' + accessToken
@@ -194,3 +194,22 @@ function PlaylistAnalyzer({ accessToken }) {
 }
 
 export default PlaylistAnalyzer;
+
+
+// Helper function to fetch with retries
+const fetchWithRetries = async (url, options, retries = 3) => {
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok && retries > 0) {
+            const retryAfter = response.headers.get('Retry-After');
+            if (retryAfter) {
+                console.log(`Rate limited. Retrying after ${retryAfter} seconds.`);
+                await new Promise(resolve => setTimeout(resolve, retryAfter * 1000));
+                return fetchWithRetries(url, options, retries - 1);
+            }
+        }
+        return response;
+    } catch (error) {
+        throw new Error('Request failed.');
+    }
+};
