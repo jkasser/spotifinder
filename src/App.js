afb8a1e15f1be9adfcad7b713df93133010d4697
jkasser <jkasser@gmail.com>
2024-04-02 12:09:35 -0400

    fix build issues

--- src/App.js
+++ src/App.js
@@ -5,26 +5,53 @@ import PlaylistAnalyzer from "./components/AnalyzerForm";
 function App() {
     const [accessToken, setAccessToken] = useState('');
 
-    const sha256 = async (plain) => {
-        const encoder = new TextEncoder()
-        const data = encoder.encode(plain)
-        return window.crypto.subtle.digest('SHA-256', data)
-    }
-
-    const base64encode = (input) => {
-        return btoa(String.fromCharCode(...new Uint8Array(input)))
-            .replace(/=/g, '')
-            .replace(/\+/g, '-')
-            .replace(/\//g, '_');
-    }
-
     useEffect(() => {
+        const sha256 = async (plain) => {
+            const encoder = new TextEncoder()
+            const data = encoder.encode(plain)
+            return window.crypto.subtle.digest('SHA-256', data)
+        }
+
+        const generateCodeChallenge = (codeVerifier) => {
+            const base64encode = (input) => {
+                return btoa(String.fromCharCode(...new Uint8Array(input)))
+                    .replace(/=/g, '')
+                    .replace(/\+/g, '-')
+                    .replace(/\//g, '_');
+            }
+            return base64encode(sha256(codeVerifier));
+        }
+
+        const handleRedirectCallback = async () => {
+            const params = new URLSearchParams(window.location.search);
+            const code = params.get('code');
+            const state = params.get('state');
+
+            const storedState = localStorage.getItem('spotify_auth_state');
+
+            if (state === null || state !== storedState) {
+                console.error('State mismatch or missing');
+                return;
+            }
+
+            localStorage.removeItem('spotify_auth_state');
+
+            const tokenResponse = await requestBearerToken(code);
+            if (tokenResponse !== null) {
+                const { accessToken, expiresIn } = tokenResponse;
+                setAccessToken(accessToken);
+
+                document.cookie = `accessToken=${accessToken}; max-age=${expiresIn}; path=/;`;
+            } else {
+                console.error('Error exchanging authorization code for bearer token');
+            }
+        }
+
         const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
         const redirectUri = process.env.REACT_APP_REDIRECT_URI;
         const scopes = encodeURIComponent('playlist-read-private');
         const state = encodeURIComponent(generateRandomString(16));
         const codeVerifier = generateRandomString(128);
-
         const codeChallenge = generateCodeChallenge(codeVerifier);
 
         const url = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scopes}&state=${state}&code_challenge_method=S256&code_challenge=${codeChallenge}`;
@@ -34,32 +61,7 @@ function App() {
         } else {
             window.location.href = url;
         }
-    }, [generateCodeChallenge]);
-
-    async function handleRedirectCallback() {
-        const params = new URLSearchParams(window.location.search);
-        const code = params.get('code');
-        const state = params.get('state');
-
-        const storedState = localStorage.getItem('spotify_auth_state');
-
-        if (state === null || state !== storedState) {
-            console.error('State mismatch or missing');
-            return;
-        }
-
-        localStorage.removeItem('spotify_auth_state');
-
-        const tokenResponse = await requestBearerToken(code);
-        if (tokenResponse !== null) {
-            const { accessToken, expiresIn } = tokenResponse;
-            setAccessToken(accessToken);
-
-            document.cookie = `accessToken=${accessToken}; max-age=${expiresIn}; path=/;`;
-        } else {
-            console.error('Error exchanging authorization code for bearer token');
-        }
-    }
+    }, []); // Empty dependency array ensures this effect runs only once
 
     function generateRandomString(length) {
         const possibleChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -68,10 +70,6 @@ function App() {
             randomString += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
         }
         return randomString;
-    }
-
-    function generateCodeChallenge(codeVerifier) {
-        return base64encode(sha256(codeVerifier));
     }
 
     return (
